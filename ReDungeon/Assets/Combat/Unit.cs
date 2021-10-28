f352558023b5ca981d47b99a2b5c63139f693fb1
Michail <misah.pletneow@yandex.ru>
2021-10-28 15:52:14 +0300

    Add Action.cs, Expand Unit.cs

--- ReDungeon/Assets/Combat/Unit.cs
+++ ReDungeon/Assets/Combat/Unit.cs
@@ -12,14 +12,12 @@ public abstract class Unit : MonoBehaviour
     public int currentHP;
     public int attackBase;
     public int attackBonusAdd;
-    public int attackBonusMult;
+    public float attackBonusMult;
     public int defenceBase;
     public int defenceBonusAdd;
-    public int defenceBonusMult;
-    public int damageReduction;
+    public float damageReduction;
     public int recoil;
-    // TODO: Make Action class which holds an active skill: it's name, description, stats, function e.t.c. it shouldn't include any tree parts. They will be parts of the wrapper.
-    //public List<Action> actions; 
+    public List<Action> actions; 
 
     delegate void onHitHandler(Unit sender, Unit attacker, int DamageBase);
     delegate void onAttackHandler(Unit sender, Unit target, int DamageBase);
@@ -28,8 +26,34 @@ public abstract class Unit : MonoBehaviour
 
     public Unit()
     {
-        setDefaults();
+        SetDefaults();
     }
 
-    public virtual void setDefaults() { }
+    public virtual void SetDefaults() { }
+    public virtual void PreTurn () { }
+    public virtual void PostTurn() { }
+    public void Hurt(int Damage)
+    {
+        int dealtDamage = Mathf.Max(0,Mathf.RoundToInt(Damage / damageReduction - defenceBase - defenceBonusAdd));
+        currentHP = Mathf.Max(0,currentHP - dealtDamage);
+        if (currentHP == 0)
+        {
+            Die(); 
+        }
+    }
+    public void Die()
+    {
+
+    }
+    public virtual void ModifyHitByUnit(Action action, Unit attacker, ref int damage, ref bool crit) { }
+    public virtual void OnHitByUnit(Action action, Unit attacker, int damage, bool crit) 
+    { 
+        ModifyHitByUnit(action, attacker, ref damage, ref crit);
+    }
+    public virtual void ModifyHitUnit(Action action, Unit Victim, ref int damage, ref bool crit) { }
+    public virtual void OnHitUnit(Action action, Unit Victim, ref int damage, ref bool crit) 
+    {
+        ModifyHitUnit(action, Victim, ref damage, ref crit);
+        Victim.OnHitByUnit(action, this, damage, crit);
+    }
 }
--- ReDungeon/Assets/Combat/UnitWrapper.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlayerUnit : Unit
-{
-    //public List<Skill> availableSkills;
-    //public List<Action> availableActions;
-}
