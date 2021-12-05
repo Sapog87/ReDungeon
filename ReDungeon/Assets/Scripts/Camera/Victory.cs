ccf99715d6f38572b8c403d819d72b398df602a3
Sapog87 <73855149+Sapog87@users.noreply.github.com>
2021-12-05 18:29:26 +0300

    Small modification

--- ReDungeon/Assets/Scripts/Camera/Victory.cs
+++ ReDungeon/Assets/Scripts/Camera/Victory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class Return : MonoBehaviour
+public class Victory : MonoBehaviour
 {
     void Start()
     {
