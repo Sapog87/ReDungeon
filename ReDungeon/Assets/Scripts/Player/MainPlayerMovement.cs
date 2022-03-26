0de7eca95ee58c351309521b78b7fca35c002866
entcher <49116032+entcher@users.noreply.github.com>
2022-03-26 17:43:08 +0300

    Add player animations

--- ReDungeon/Assets/Scripts/Player/MainPlayerMovement.cs
+++ ReDungeon/Assets/Scripts/Player/MainPlayerMovement.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class MainPlayerMovement : MonoBehaviour
 {
+    Animator animator;
+
     [Header("Parameters")]
     [SerializeField] private GameObject mainPlayer;
     [SerializeField] private string playerTag;
     [SerializeField] private float speed;
-    
+
 
     void Awake()
     {
         playerTag = "Player";
         mainPlayer = GameObject.FindGameObjectWithTag(playerTag);
         speed = 10f;
+
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -26,6 +30,10 @@ public class MainPlayerMovement : MonoBehaviour
 
         mainPlayer.transform.Translate(Vector2.up * speed * yPos * Time.fixedDeltaTime);
         mainPlayer.transform.Translate(Vector2.right * speed * xPos * Time.fixedDeltaTime);
+
+        animator.SetFloat("Horizontal", xPos);
+        animator.SetFloat("Vertical", yPos);
+        animator.SetBool("IsMoving", xPos != 0 || yPos != 0);
     }
     public void StopPlayer()
     {
