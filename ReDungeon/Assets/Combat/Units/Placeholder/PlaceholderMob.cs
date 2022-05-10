f691a2b275bb16529336e34e454eaa236a282219
Oleg <gthxbr2004@gmail.com>
2022-05-10 22:22:56 +0300

    Edit placeholders. Edit spawners.

--- ReDungeon/Assets/Combat/Units/Placeholder/PlaceholderMob.cs
+++ ReDungeon/Assets/Combat/Units/Placeholder/PlaceholderMob.cs
@@ -8,10 +8,14 @@ public class PlaceholderMob : Unit
     public override void SetDefaults()
     {
         Actions.Add(new SimpleLick());
+        Actions.Add(new Sandstorm());
+        Actions.Add(new FlexTape());
+        Actions.Add(new Scratch());
+        Actions.Add(new BoxRam());
     }
 
     public override Action Ai(UnitObject[] allies, UnitObject[] opponents)
     {
-        return Actions[0];
+        return Actions[Random.Range(0, Actions.Count)];
     }
 }
